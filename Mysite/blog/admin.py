bc8f816717eebe864347ce8af27f577471d8a90a
Amir20Hossein <13gameplay83@gmail.com>
2024-08-21 01:46:30 +0330

    .

--- Mysite/blog/admin.py
+++ Mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+#@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty'
+    list_display = ('title','conted_views','created_date','status',)
+    list_filter = ('status',)
+    search_fields = ('title', 'content',)
+    
+admin.site.register(Post, PostAdmin)
--- Mysite/blog/models.py
+++ Mysite/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    #images
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #tag
+    # category
+    conted_views = models.IntegerField(default=0)
+    status = models.BooleanField()
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_date']
+    def __str__(self):
+        return " {} {} ".format(self.title,self.id)
