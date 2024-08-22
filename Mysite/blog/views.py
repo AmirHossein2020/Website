d77c5fc6763c32970488ed3424dd18eb2f744a14
Amir20Hossein <13gameplay83@gmail.com>
2024-08-22 22:46:55 +0330

    .

--- Mysite/blog/views.py
+++ Mysite/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
 def blogView(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html',context)
 
 def SingleView(request):
     return render(request, 'blog/blog-single.html')
+
