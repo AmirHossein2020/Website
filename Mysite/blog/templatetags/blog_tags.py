81613e7fa9492e5c911f2264642aa8a149a65b9b
Amir20Hossein <13gameplay83@gmail.com>
2024-08-30 02:42:49 +0330

    .

--- Mysite/blog/templatetags/blog_tags.py
+++ Mysite/blog/templatetags/blog_tags.py
@@ -12,3 +12,8 @@ def post_category():
         cat_dict[name] = posts.filter(category=name).count()
 
     return {'categorys': cat_dict}
+
+@register.inclusion_tag('blog/blog-post-p.html')
+def latest_post():
+    posts = Post.objects.filter(status=1).order_by('published_date')[:2]
+    return {'posts': posts}
