7e2da5db9dc8b9b5cef4cb29bcd9e6c5e52e285c
Amir20Hossein <13gameplay83@gmail.com>
2024-08-30 09:20:44 +0330

    .

--- Estate/web/urls.py
+++ Estate/web/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from web.views import HomePage , category , category2
+from web.views import HomePage , category , category_type
 
 urlpatterns = [
    path('', HomePage,name='home'),
    path('category/<str:cat>/', category, name="category"),
-
+   path('cat/<str:pid>', category_type, name="category_type"),
 ]
--- Estate/web/views.py
+++ Estate/web/views.py
@@ -7,7 +7,11 @@ def HomePage(request):
     return render(request, 'index.html')
 
 def category(request, cat):
-    category1 = HouseModel.objects.get(name= cat)
-    file = HomeFile.objects.filter(model = category1)
-    return render(request, 'listhome.html', {'file': file, 'category': category1})
+    category = HouseModel.objects.get(name= cat)
+    file = HomeFile.objects.filter(model = category)
+    return render(request, 'listhome.html', {'file': file, 'category': category})
 
+def category_type(request, pid):
+    category_type = HouseCategory.objects.filter(type= pid)
+    file = HomeFile.objects.filter(type = category_type)
+    return render(request, 'listhome.html', {'file_type': file, 'category_type': category_type})
