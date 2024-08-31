6dbff5758d0ba92216d67ad04a0446141edc3e58
Amir20Hossein <13gameplay83@gmail.com>
2024-08-31 03:54:37 +0330

    .

--- Mysite/blog/urls.py
+++ Mysite/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import blogView , SingleView
+from .views import blogView , SingleView , blog_category
 
 
 urlpatterns = [
    path('home/', blogView, name='blog' ),
    path('<int:pid>', SingleView, name='single' ),
+   path('category/<str:cat_name>', blog_category, name='category' ),
 ]
--- Mysite/blog/views.py
+++ Mysite/blog/views.py
@@ -14,3 +14,9 @@ def SingleView(request,pid):
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
 
+
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name = cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html',context)
