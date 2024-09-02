1aaac728af824a852ece2e241459028b9e8d6477
Amir20Hossein <13gameplay83@gmail.com>
2024-09-02 21:06:26 +0330

    .

--- Mysite/blog/urls.py
+++ Mysite/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import blogView , SingleView , blog_category
+from .views import *
 
 
 urlpatterns = [
    path('home/', blogView, name='blog' ),
    path('<int:pid>', SingleView, name='single' ),
+   path('author/<str:author_username>', blogView, name='author' ),
+   path('search/', blog_search, name='search' ),
    path('category/<str:cat_name>', blog_category, name='category' ),
 ]
--- Mysite/blog/views.py
+++ Mysite/blog/views.py
@@ -3,8 +3,12 @@ from blog.models import Post
 
 
 # Create your views here.
-def blogView(request):
+def blogView(request,cat_name=None,author_username=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
+    if author_username:
+        posts = posts.filter(author__username = author_username)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html',context)
 
@@ -20,3 +24,12 @@ def blog_category(request, cat_name):
     posts = posts.filter(category__name = cat_name)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html',context)
+
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
