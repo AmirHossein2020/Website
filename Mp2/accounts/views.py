c34627b86a62164939c5aa02585b62185124190e
Amir Hossein Amjadian <13gameplay83@gmail.com>
2025-05-24 16:39:21 +0330

    Update views.py

--- Mp2/accounts/views.py
+++ Mp2/accounts/views.py
@@ -9,6 +9,34 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
+
+def ProfileEditView(request):
+    
+    if request.method=="POST":
+        profileEditForm=ProfileEditForm(request.POST,request.FILES, instance=request.user.profile)
+        userEditForm=UserEditForm(request.POST,instance=request.user)
+        if profileEditForm.is_valid and userEditForm.is_valid:
+            profileEditForm.save()
+            userEditForm.save()
+            return HttpResponseRedirect(reverse(profileView))
+    else:
+        profileEditForm=ProfileEditForm(instance=request.user.profile)
+        userEditForm=UserEditForm(instance=request.user)
+
+    context={
+
+        "profileEditForm":profileEditForm,
+        "userEditForm":userEditForm,
+        "ProfileImage":request.user.profile.ProfileImage,
+        
+    }
+
+    return render(request,"accounts/profileEdit.html",context)
+
+
+
+
+
 """ def SignUpView(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
