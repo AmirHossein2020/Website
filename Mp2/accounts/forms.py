261e7828998a77e73a83aa72ebd82f099f69af05
Amir Hossein Amjadian <13gameplay83@gmail.com>
2025-05-24 16:41:01 +0330

    Update forms.py

--- Mp2/accounts/forms.py
+++ Mp2/accounts/forms.py
@@ -7,3 +7,14 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('email','address','phon','post_code')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ProfileModel
+        fields = ['ProfileImage', 'Credit', 'Gender']
+
+class UserEditForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        fields=["first_name","last_name","email"]
+    password=None
