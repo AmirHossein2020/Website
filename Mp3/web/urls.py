642443d0318bdc30fec19f5c87e40e5e0d9b925c
Amir20Hossein <13gameplay83@gmail.com>
2025-06-16 14:28:25 +0330

    start Mp3

---
+++ Mp3/web/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from web.views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('products/', products, name='products'),
+    path('about/', about, name='about'),
+    path('client/', client, name='client'),
+    path('contact/', contact, name='contact'),
+]
